7512f742942c05697ae442d5984bd309df4d25e6
1Tsw0rd <31820402+SeDevel@users.noreply.github.com>
2023-01-05 11:59:36 +0900

    레포지토리 테스트 - 책삭제하기 버그수정

--- src/test/java/site/metacoding/junittest/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junittest/domain/BookRepositoryTest.java
@@ -1,10 +1,16 @@
 package site.metacoding.junittest.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest //DB와 관련된 컴포넌트만 메모리에 로딩, 컨트롤러와 서비스는 메모리에 뜨지 않음
 public class BookRepositoryTest {
@@ -12,6 +18,25 @@ public class BookRepositoryTest {
 
     @Autowired //DI 테스트할 땐 @Autowired 쓰는게 좋음
     private BookRepository bookRepository;
+
+    //@BeforeAll //테스트 시작 전에 한번만 실행
+    @BeforeEach //각 테스트 시작전에 한번씩 실행
+    public void 데이터준비(){
+        // 잘 되는지 체크System.out.println("===================");
+        String title = "junit";
+        String author = "겟인데어";
+
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        bookRepository.save(book);
+
+    } //트랜잭션 종료 됐다면 말이 안됨
+    //가정 1 : T [데이터준비() + 1 책등록], T [데이터준비() + 2. 책목록보기]  << 이렇게 동작함
+    //가정 2 : T [데이터준비() + 1 책등록 + 데이터준비() + 2.책목록보기]
+
 
     //1. 책 등록
     @Test
@@ -36,14 +61,62 @@ public class BookRepositoryTest {
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
         //assertEquals(expected, actual) 기대하는 값(title)과 실제 값(bookPS.getTile()) 비교
-    }
+    } //트랜잭션 종료(저장된 데이터 초기화)
 
     //2. 책 목록보기
+    @Test
+    public void 책목록보기_test(){
+        //given
+        String title = "junit";
+        String author = "겟인데어";
+
+        //when
+        List<Book> booksPS= bookRepository.findAll();
+
+        System.out.println("사이즈 ========================= :" + booksPS.size());
+
+        //then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+    }//트랜잭션 종료(저장된 데이터 초기화)
 
     //3. 책 한건 보기
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책한건보기_test() {
+        //given
+        String title = "junit";
+        String author = "겟인데어";
+
+        //when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        //then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    }//트랜잭션 종료(저장된 데이터 초기화)
+
 
     //4. 책 수정
 
+
     //5. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책삭제_test(){
+        //given
+        Long id = 1L;
+        //when
+        bookRepository.deleteById(id);
+
+        //then
+        //Optional<Book> bookPS1 = bookRepository.findById(id);
+        //Optional은 null값도 받음
+
+        assertFalse(bookRepository.findById(id).isPresent());
+        //delete는 반환되는 값이 없어야 성공
+        //assertFalse()는 false 일 때 true
+        //isPresent()는 값이 없으면 true 반환(값 있니?)
+    }
     
 }
